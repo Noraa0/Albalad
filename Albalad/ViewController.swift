988148b1ace6f47418203283a33e9afe6407f6c9
Nora alluqmani <nuraalluqmani@gmail.com>
2022-01-02 09:09:16 +0300

    first edit

---
+++ Albalad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Albalad
+//
+//  Created by Nora on 28/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
